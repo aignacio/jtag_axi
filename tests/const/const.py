983f455fb30e20a70a2fbc2dff513026cb4f76d5
Anderson Ignacio <anderson@aignacio.com>
2024-08-23 21:03:15 +0100

    Initial commit

---
+++ tests/const/const.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : const.py
+# License           : MIT license <Check LICENSE>
+# Author            : Anderson Ignacio da Silva (aignacio) <anderson@aignacio.com>
+# Date              : 12.07.2023
+# Last Modified Date: 23.08.2024
+import os
+import glob
+
+
+class cfg:
+    RST_CYCLES = 3
+    CLK_100MHz = (10, "ns")
+    TIMEOUT_TEST = (CLK_100MHz[0] * 200, "ns")
+
+    TOPLEVEL = str(os.getenv("DUT"))
+    SIMULATOR = str(os.getenv("SIM"))
+
+    TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+    INC_DIR = [os.path.join(TESTS_DIR, "../../rtl/include")]
+    RTL_DIR = os.path.join(TESTS_DIR, "../../rtl")
+
+    VERILOG_SOURCES = []  # The sequence below is important...
+    VERILOG_SOURCES = VERILOG_SOURCES + glob.glob(f"{RTL_DIR}/*.v", recursive=True)
+    VERILOG_SOURCES = VERILOG_SOURCES + glob.glob(f"{RTL_DIR}/*.sv", recursive=True)
+
+    EXTRA_ENV = {}
+    EXTRA_ENV["COCOTB_HDL_TIMEPRECISION"] = os.getenv("TIMEPREC")
+    EXTRA_ENV["COCOTB_HDL_TIMEUNIT"] = os.getenv("TIMEUNIT")
+    TIMESCALE = os.getenv("TIMEUNIT") + "/" + os.getenv("TIMEPREC")
+
+    if SIMULATOR == "verilator":
+        EXTRA_ARGS = [
+            "--trace-fst",
+            "--coverage",
+            "--coverage-line",
+            "--coverage-toggle",
+            "--trace-structs",
+        ]
+        PLUS_ARGS = ["--trace"]
+    else:
+        EXTRA_ARGS = []
