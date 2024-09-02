2669e625e58541a864a98d449406ce8fa0245a49
Anderson Ignacio da Silva <anderson@aignacio.com>
2024-09-02 19:07:39 +0100

    Script working
    Signed-off-by: Anderson Ignacio da Silva <anderson@aignacio.com>

--- jtag_axi/jtag_to_axi.py
+++ jtag_axi/jtag_to_axi.py
@@ -4,7 +4,7 @@
 # License           : MIT license <Check LICENSE>
 # Author            : Anderson I. da Silva (aignacio) <anderson@aignacio.com>
 # Date              : 29.08.2024
-# Last Modified Date: 31.08.2024
+# Last Modified Date: 02.09.2024
 from pyftdi.ftdi import Ftdi
 from pyftdi.jtag import JtagEngine, JtagTool
 from os import environ
@@ -18,7 +18,9 @@ JTAG_INSTR = {'SAMPLE': BitSequence('0001', msb=True, length=4),
               'PRELOAD': BitSequence('0001', msb=True, length=4),
               'IDCODE': BitSequence('0100', msb=True, length=4),
               'BYPASS': BitSequence('1111', msb=True, length=4),
-              'ADDR_REGISTER': BitSequence('1101', msb=True, length=4)}
+              'ADDR_AXI_REGISTER': BitSequence('0001', msb=True, length=4),
+              'DATA_AXI_REGISTER': BitSequence('0010', msb=True, length=4),
+              'MGMT_AXI_REGISTER': BitSequence('0011', msb=True, length=4)}
 # Replace this URL with the one that matches your FT2232 device
 FTDI_DEVICE_URL = 'ftdi://ftdi:2232/1'
 
@@ -32,30 +34,31 @@ jtag = JtagEngine(trst=False, frequency=1E6)
 jtag.configure(url)
 jtag.reset()
 
-# """Read the IDCODE right after a JTAG reset"""
-# jtag.reset()
-# idcode = jtag.read_dr(32)
-# print(f'IDCODE (reset): 0x{int(idcode):x}')
+"""Read the IDCODE right after a JTAG reset"""
+jtag.reset()
+idcode = jtag.read_dr(32)
+print(f'IDCODE (reset): 0x{int(idcode):x}')
 
-# """Test the BYPASS instruction using shift_and_update_register"""
-# instruction = JTAG_INSTR['BYPASS']
-# jtag.change_state('shift_ir')
-# retval = jtag.shift_and_update_register(instruction)
-# print(f'retval: 0x{int(retval):x}')
-# jtag.go_idle()
-# jtag.change_state('shift_dr')
-# in_ = BitSequence('011011110000'*2, length=24)
-# out = jtag.shift_and_update_register(in_)
+"""Test the BYPASS instruction using shift_and_update_register"""
+instruction = JTAG_INSTR['BYPASS']
+jtag.change_state('shift_ir')
+retval = jtag.shift_and_update_register(instruction)
+print(f'retval: 0x{int(retval):x}')
+jtag.go_idle()
+jtag.change_state('shift_dr')
+in_ = BitSequence('011011110000'*2, length=24)
+out = jtag.shift_and_update_register(in_)
+print(f'BYPASS sent:\n{in_}\n{out} '
+      f' (should be left shifted by one)')
 
 
-instruction = JTAG_INSTR['ADDR_REGISTER']
+instruction = JTAG_INSTR['ADDR_AXI_REGISTER']
 jtag.change_state('shift_ir')
 # jtag.write_ir(instruction)
 ret = jtag.shift_and_update_register(instruction)
 print(ret)
 addr_on = BitSequence('10000000000000000000000000000001', msb=True, length=32)
 addr_off = BitSequence('00000000000000000000000000000000', msb=True, length=32)
-# jtag.change_state('shift_dr')
 
 for i in range(100000):
     print(f"Sending JTAG command no {i}")
